9b0089e0df443213e0ea77a533ca120a36b056fd
cchantep <chantepie@altern.org>
2014-07-28 14:17:04 +0200

    Model improvements

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,8 +4,8 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-
-import anorm._
+import play.api.db.DB
+import play.api.Play.current
 
 import views._
 import models._
@@ -47,11 +47,12 @@ object Application extends Controller {
    * @param orderBy Column to be sorted
    * @param filter Filter applied on computer names
    */
-  def list(page: Int, orderBy: Int, filter: String) = Action { implicit request =>
-    Ok(html.list(
-      Computer.list(page = page, orderBy = orderBy, filter = ("%"+filter+"%")),
-      orderBy, filter
-    ))
+  def list(page: Int, orderBy: Int, filter: String) = Action { implicit req => 
+    DB withConnection { implicit con =>
+      Ok(html.list(
+        Computer.list(page = page, orderBy = orderBy, 
+          filter = ("%"+filter+"%")), orderBy, filter))
+    }
   }
   
   /**
@@ -60,9 +61,11 @@ object Application extends Controller {
    * @param id Id of the computer to edit
    */
   def edit(id: Long) = Action {
-    Computer.findById(id).map { computer =>
-      Ok(html.editForm(id, computerForm.fill(computer), Company.options))
-    }.getOrElse(NotFound)
+    DB withConnection { implicit con =>
+      Computer.findById(id).fold[Result](NotFound) { computer =>
+        Ok(html.editForm(id, computerForm.fill(computer), Company.options))
+      }
+    }
   }
   
   /**
@@ -71,42 +74,49 @@ object Application extends Controller {
    * @param id Id of the computer to edit
    */
   def update(id: Long) = Action { implicit request =>
-    computerForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(html.editForm(id, formWithErrors, Company.options)),
-      computer => {
-        Computer.update(id, computer)
-        Home.flashing("success" -> "Computer %s has been updated".format(computer.name))
-      }
-    )
+    DB withConnection { implicit con =>
+      computerForm.bindFromRequest.fold(formWithErrors =>
+        BadRequest(html.editForm(id, formWithErrors, Company.options)),
+        computer => {
+          Computer.update(id, computer)
+          Home.flashing("success" -> 
+            s"Computer ${computer.name} has been updated")
+        })
+    }
   }
   
   /**
    * Display the 'new computer form'.
    */
   def create = Action {
-    Ok(html.createForm(computerForm, Company.options))
+    DB withConnection { implicit con => 
+      Ok(html.createForm(computerForm, Company.options))
+    }
   }
   
   /**
    * Handle the 'new computer form' submission.
    */
   def save = Action { implicit request =>
-    computerForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(html.createForm(formWithErrors, Company.options)),
+    DB withConnection { implicit con =>
+      computerForm.bindFromRequest.fold(formWithErrors => 
+        BadRequest(html.createForm(formWithErrors, Company.options)),
       computer => {
         Computer.insert(computer)
-        Home.flashing("success" -> "Computer %s has been created".format(computer.name))
-      }
-    )
+        Home.flashing("success" -> 
+          s"Computer ${computer.name} has been created")
+      })
+    }
   }
   
   /**
    * Handle computer deletion.
    */
   def delete(id: Long) = Action {
-    Computer.delete(id)
-    Home.flashing("success" -> "Computer has been deleted")
+    DB withConnection { implicit con =>
+      Computer.delete(id)
+      Home.flashing("success" -> "Computer has been deleted")
+    }
   }
-
 }
             
--- app/models/Models.scala
+++ app/models/Models.scala
@@ -1,18 +1,19 @@
 package models
 
-import java.util.{Date}
+import java.util.Date
+import java.sql.Connection
 
-import play.api.db._
-import play.api.Play.current
-
-import anorm._
-import anorm.SqlParser._
-
-import scala.language.postfixOps
+import anorm.{ SqlStringInterpolation, ~ }
+import anorm.SqlParser.{ date, long, scalar, str }
 
 case class Company(id: Option[Long] = None, name: String)
-case class Computer(id: Option[Long] = None, name: String, introduced: Option[Date], discontinued: Option[Date], companyId: Option[Long])
 
+case class Computer(
+  id: Option[Long] = None, 
+  name: String, 
+  introduced: Option[Date], 
+  discontinued: Option[Date], 
+  companyId: Option[Long])
 
 /**
  * Helper for pagination.
@@ -30,20 +31,21 @@ object Computer {
    * Parse a Computer from a ResultSet
    */
   val simple = {
-    get[Option[Long]]("computer.id") ~
-    get[String]("computer.name") ~
-    get[Option[Date]]("computer.introduced") ~
-    get[Option[Date]]("computer.discontinued") ~
-    get[Option[Long]]("computer.company_id") map {
-      case id~name~introduced~discontinued~companyId => Computer(id, name, introduced, discontinued, companyId)
+    long("computer.id").? ~
+    str("computer.name") ~
+    date("computer.introduced").? ~
+    date("computer.discontinued").? ~
+    long("computer.company_id").? map {
+      case id ~ name ~ introduced ~ discontinued ~ companyId => 
+        Computer(id, name, introduced, discontinued, companyId)
     }
   }
   
   /**
    * Parse a (Computer,Company) from a ResultSet
    */
-  val withCompany = Computer.simple ~ (Company.simple ?) map {
-    case computer~company => (computer,company)
+  val withCompany = Computer.simple ~ Company.simple.? map {
+    case computer ~ company => (computer, company)
   }
   
   // -- Queries
@@ -51,55 +53,36 @@ object Computer {
   /**
    * Retrieve a computer from the id.
    */
-  def findById(id: Long): Option[Computer] = {
-    DB.withConnection { implicit connection =>
-      SQL("select * from computer where id = {id}").on('id -> id).as(Computer.simple.singleOpt)
-    }
-  }
+  def findById(id: Long)(implicit con: Connection): Option[Computer] = 
+    SQL"select * from computer where id = $id" as Computer.simple.singleOpt
   
   /**
-   * Return a page of (Computer,Company).
+   * Return a page of (Computer, Company).
    *
    * @param page Page to display
    * @param pageSize Number of computers per page
    * @param orderBy Computer property used for sorting
    * @param filter Filter applied on the name column
    */
-  def list(page: Int = 0, pageSize: Int = 10, orderBy: Int = 1, filter: String = "%"): Page[(Computer, Option[Company])] = {
+  def list(page: Int = 0, pageSize: Int = 10, orderBy: Int = 1, filter: String = "%")(implicit con: Connection): Page[(Computer, Option[Company])] = {
     
-    val offest = pageSize * page
+    val offset = pageSize * page
     
-    DB.withConnection { implicit connection =>
-      
-      val computers = SQL(
-        """
-          select * from computer 
-          left join company on computer.company_id = company.id
-          where computer.name like {filter}
-          order by {orderBy} nulls last
-          limit {pageSize} offset {offset}
-        """
-      ).on(
-        'pageSize -> pageSize, 
-        'offset -> offest,
-        'filter -> filter,
-        'orderBy -> orderBy
-      ).as(Computer.withCompany *)
+    val computers = SQL"""
+        select * from computer 
+        left join company on computer.company_id = company.id
+        where computer.name like $filter
+        order by $orderBy nulls last
+        limit $pageSize offset $offset
+      """ as Computer.withCompany.*
 
-      val totalRows = SQL(
-        """
-          select count(*) from computer 
-          left join company on computer.company_id = company.id
-          where computer.name like {filter}
-        """
-      ).on(
-        'filter -> filter
-      ).as(scalar[Long].single)
+    val totalRows = SQL"""
+        select count(*) from computer 
+        left join company on computer.company_id = company.id
+        where computer.name like $filter
+      """ as scalar[Long].single
 
-      Page(computers, page, offest, totalRows)
-      
-    }
-    
+    Page(computers, page, offset, totalRows)
   }
   
   /**
@@ -108,57 +91,33 @@ object Computer {
    * @param id The computer id
    * @param computer The computer values.
    */
-  def update(id: Long, computer: Computer) = {
-    DB.withConnection { implicit connection =>
-      SQL(
-        """
-          update computer
-          set name = {name}, introduced = {introduced}, discontinued = {discontinued}, company_id = {company_id}
-          where id = {id}
-        """
-      ).on(
-        'id -> id,
-        'name -> computer.name,
-        'introduced -> computer.introduced,
-        'discontinued -> computer.discontinued,
-        'company_id -> computer.companyId
-      ).executeUpdate()
-    }
-  }
+  def update(id: Long, computer: Computer)(implicit con: Connection): Int = 
+    SQL"""
+        update computer set name = ${computer.name}, 
+        introduced = ${computer.introduced}, 
+        discontinued = ${computer.discontinued}, 
+        company_id = ${computer.companyId} where id = $id
+      """.executeUpdate()
   
   /**
    * Insert a new computer.
    *
    * @param computer The computer values.
    */
-  def insert(computer: Computer) = {
-    DB.withConnection { implicit connection =>
-      SQL(
-        """
-          insert into computer values (
-            (select next value for computer_seq), 
-            {name}, {introduced}, {discontinued}, {company_id}
-          )
-        """
-      ).on(
-        'name -> computer.name,
-        'introduced -> computer.introduced,
-        'discontinued -> computer.discontinued,
-        'company_id -> computer.companyId
-      ).executeUpdate()
-    }
-  }
+  def insert(computer: Computer)(implicit con: Connection): Int = 
+    SQL"""
+      insert into computer values (
+        (select next value for computer_seq), ${computer.name}, 
+        ${computer.introduced}, ${computer.discontinued}, ${computer.companyId})
+      """.executeUpdate()
   
   /**
    * Delete a computer.
    *
    * @param id Id of the computer to delete.
    */
-  def delete(id: Long) = {
-    DB.withConnection { implicit connection =>
-      SQL("delete from computer where id = {id}").on('id -> id).executeUpdate()
-    }
-  }
+  def delete(id: Long)(implicit con: Connection): Int = 
+    SQL"delete from computer where id = $id".executeUpdate()
   
 }
 
@@ -167,22 +126,18 @@ object Company {
   /**
    * Parse a Company from a ResultSet
    */
-  val simple = {
-    get[Option[Long]]("company.id") ~
-    get[String]("company.name") map {
-      case id~name => Company(id, name)
-    }
+  val simple = long("company.id").? ~ str("company.name") map {
+    case id ~ name => Company(id, name)
   }
   
   /**
    * Construct the Map[String,String] needed to fill a select options set.
    */
-  def options: Seq[(String,String)] = DB.withConnection { implicit connection =>
-    SQL("select * from company order by name").as(Company.simple *).
-      foldLeft[Seq[(String, String)]](Nil) { (cs, c) => 
+  def options(implicit con: Connection): Seq[(String,String)] = 
+    SQL"select * from company order by name".as(Company.simple.*).
+      foldLeft(Seq[(String, String)]()) { (cs, c) =>
         c.id.fold(cs) { id => cs :+ (id.toString -> c.name) }
       }
-  }
-  
+
 }
 
