269ad3a14d308c872afee4dc1ac88fd0a131d7c3
Grzegorz Slowikowski <gslowikowski@gmail.com>
2017-03-18 17:00:00 +0100

    Don't use date-type inputs (<input type="date">)
    They're not supported by all brwsers, see http://caniuse.com/#feat=input-datetime

--- test/FunctionalSpec.scala
+++ test/FunctionalSpec.scala
@@ -49,7 +49,7 @@ class FunctionalSpec extends PlaySpec with OneAppPerSuite with ScalaFutures {
 
       status(badDateFormat) must equal(BAD_REQUEST)
       contentAsString(badDateFormat) must include("""<option value="1" selected="selected">Apple Inc.</option>""")
-      contentAsString(badDateFormat) must include("""<input type="date" id="introduced" name="introduced" value="badbadbad" """)
+      contentAsString(badDateFormat) must include("""<input type="text" id="introduced" name="introduced" value="badbadbad" """)
       contentAsString(badDateFormat) must include("""<input type="text" id="name" name="name" value="FooBar" """)
 
 
